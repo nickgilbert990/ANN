29793801aec36a9c7a49437f31c78d497bf45548
Nick Gilbert <nickgilbert990@gmail.com>
2019-10-23 11:27:08 +0100

    Added integration test project (currently blank) and fixed a typo in the unit test project name

---
+++ NeuralNetwork/IntegrationTestNeuralNetwork/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IntegrationTestNeuralNetwork")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("IntegrationTestNeuralNetwork")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2bcd8959-009a-4320-a5ab-916451bcc497")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
